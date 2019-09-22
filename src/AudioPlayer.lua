d90d1a92128f1b6a0065d5882fe1188f0efbbb2b
Harine Choi <harinechoi@Harines-MBP.wv.cc.cmu.edu>
2019-09-22 15:08:52 -0400

    Added filter to filteredAudio

--- src/AudioPlayer.lua
+++ src/AudioPlayer.lua
@@ -1,9 +1,17 @@
 AudioPlayer = {}
 AudioPlayer.__index = AudioPlayer
 
-function AudioPlayer:init()
+function AudioPlayer:init(audio)
     local o = o or {}   -- create object if user does not provide one
     setmetatable(o, self)
     self.__index = self
+    self.audio = audio:clone()
+    self.filteredAudio = audio:clone()
+
+    self.filteredAudio:setFilter{
+        type = "lowpass",
+        volume = 1,
+        highgain = .5,
+    }
     return o
 end
