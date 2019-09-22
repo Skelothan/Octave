bd44740891ce27caba6c2d58f7fdef97942191bb
williamozeas <49919334+williamozeas@users.noreply.github.com>
2019-09-22 15:33:13 -0400

    Inputs handled

--- main.lua
+++ main.lua
@@ -22,6 +22,7 @@ function love.load()
 	gBackground = Background:init("oscCircle")
 	
 	love.keyboard.keysPressed = {}
+	love.keyboard.bindings = {}
 end
 
 
@@ -31,14 +32,19 @@ function love.update(dt)
 	gStateMachine:update(dt)
 	
 	love.keyboard.keysPressed = {}
+	--stores the actual inputs
+	love.keyboard.input = {}
 end
 
 function love.resize(x, y)
 	
 end
 
+--INPUT HANDLING BEGIN
 function love.keypressed(key)
 	love.keyboard.keysPressed[key] = true
+	local action = keys[key]
+	love.keyboard.inputs[action] = true;
 end
 
 --[[ 
@@ -47,9 +53,41 @@ Thus, this function lets us get key press callbacks from anywhere.
 I was taught this on an earlier version of Love though, so I've no idea if this 
 is still necessary. Might be worth testing.
 ]]
+
+
+--stores the keys and what they are bound to
+local keys = {
+	escape = "togglePauseMenu",
+	
+	--player 1
+	r = "upArrow",
+	t = "upArrow",
+	f = "downArrow",
+	g = "downArrow",
+	w = "up",
+	a = "left",
+	s = "down",
+	d = "right",
+	y = "togglePauseMenu",
+	
+	--player 2
+	space = "upArrow2",
+	z = "upArrow2",
+	c = "downArrow2",
+	v = "downArrow2",
+	up = "up2",
+	down = "down2",
+	left = "left2",
+	right = "right2",
+	x = "togglePauseMenu"
+	}
+
+
 function love.keyboard.wasPressed(key)
 	return love.keyboard.keysPressed[key] or false
 end
+	
+--INPUT HANDLING END
 
 function love.draw()
 	gBackground:render()
