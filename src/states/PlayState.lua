9e479a5a3735847024bb4d55dc47e33377667b50
Will Ozeas <willatme@me.com>
2019-10-06 14:57:36 -0400

    Putting the dots/lanes into PlayState

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -1,6 +1,38 @@
 PlayState = {}
 PlayState.__index = PlayState
 
+function newPad(self, pX, pY, pRadius, pNum)
+	table.insert(self.pads, Pad:init({
+		x = pX, 
+		y = pY, 
+		radius = pRadius
+		})
+	)
+	local lane11 = Lane:init({
+		padX = pX, 
+		padY = pY, 
+		padR = pRadius,
+		angle = (pNum+1)%8
+
+	})
+	table.insert(self.lanes, lane11)
+	local lane12 = Lane:init({
+		padX = pX, 
+		padY = pY, 
+		padR = pRadius,
+		angle = (pNum+2)%8
+	})
+	table.insert(self.lanes, lane12)
+	local lane13 = Lane:init({
+		padX = pX, 
+		padY = pY, 
+		padR = pRadius,
+		angle = (pNum+3)%8
+	})
+	table.insert(self.lanes, lane13)
+
+	end
+
 function PlayState:init()
 	local o = o or {}
 	setmetatable(o, self)
@@ -9,27 +41,43 @@ function PlayState:init()
 	
 	self.pads = {}
 	self.lanes = {}
-	
-	table.insert(self.pads, Pad:init({
-		x = love.graphics.getWidth()/2, 
-		y = love.graphics.getHeight()*3/4, 
-		radius = 20
-		})
-	)
-	local newLane = Lane:init({
-		padX = love.graphics.getWidth()/2, 
-		padY = love.graphics.getHeight()*3/4, 
-		padR = 20,
-		angle = 2
-	})
-	table.insert(self.lanes, newLane)
-	local newLane2 = Lane:init({
-		padX = love.graphics.getWidth()/2, 
-		padY = love.graphics.getHeight()*3/4, 
-		padR = 20,
-		angle = 3
-	})
-	table.insert(self.lanes, newLane2)
+
+	local centerRadius = math.min(love.graphics.getHeight(), love.graphics.getWidth())/8
+	local pRadius = 20
+
+	--bottom 
+	newPad(self, love.graphics.getWidth()/2, love.graphics.getHeight()/2 + centerRadius, pRadius, 0)
+
+	--bottom left
+	newPad(self, love.graphics.getWidth()/2 - centerRadius/math.sqrt(2), 
+		love.graphics.getHeight()/2 + centerRadius/math.sqrt(2), pRadius, 1)
+
+	--left
+	newPad(self, love.graphics.getWidth()/2 - centerRadius, 
+		love.graphics.getHeight()/2, pRadius, 2)
+
+	--top left
+	newPad(self, love.graphics.getWidth()/2 - centerRadius/math.sqrt(2), 
+		love.graphics.getHeight()/2 - centerRadius/math.sqrt(2), pRadius, 3)
+
+	--top
+	newPad(self, love.graphics.getWidth()/2, 
+		love.graphics.getHeight()/2- centerRadius, pRadius, 4)
+
+	--top right
+	newPad(self, love.graphics.getWidth()/2 + centerRadius/math.sqrt(2), 
+		love.graphics.getHeight()/2 - centerRadius/math.sqrt(2), pRadius, 5)
+
+	--right
+	newPad(self, love.graphics.getWidth()/2 + centerRadius, 
+		love.graphics.getHeight()/2, pRadius, 6)
+
+	--bottom right
+	newPad(self, love.graphics.getWidth()/2 + centerRadius/math.sqrt(2), 
+		love.graphics.getHeight()/2 + centerRadius/math.sqrt(2), pRadius, 7)
+
+
+
 	
 	return table.deepcopy(o)
 end
