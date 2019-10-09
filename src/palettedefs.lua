7c193820399667d3780023e679c150aeb2d31c31
cgaffud <46664959+cgaffud@users.noreply.github.com>
2019-10-08 20:16:04 -0400

    Create palettedefs.lua

---
+++ src/palettedefs.lua
@@ -0,0 +1,14 @@
+gPalette = {
+
+-- this still needs to be implemented into bg system
+["standard"] = {
+	background = tuple(0/255, 0/255, 160/255, 255/255),
+	gradient = tuple(160/255, 0/255, 130/255, 255/255),
+	bgObjects = tuple(0/255, 0/255, 0/255, 255/255),
+	menuText = tuple(255/255, 255/255, 255/255, 255/255),	
+	scoreText = tuple(255/255, 255/255, 255/255, 255/255),
+	laneColor = tuple(60/255, 170/255, 245/255, 255/255),
+	healthColor = tuple(10/255, 240/255, 90/255, 255/255),
+	noteColor = tuple(250/255, 60/255, 140/255, 255/255)
+}
+}
