957283a176b6a05036a740fd00bffcd3ad73acda
cgaffud <46664959+cgaffud@users.noreply.github.com>
2019-10-26 16:27:54 -0400

    messing around with making new bgs

--- src/backgrounddefs.lua
+++ src/backgrounddefs.lua
@@ -17,6 +17,23 @@ function rotate_object(objCoords, angle)
 	return rotatedCoords
 end 
 
+function center_points(x, y, points)
+	allpoints = {}
+	for j, u in ipairs(points) do
+		points = {}
+		for i, v in ipairs(u) do
+			if (i%2) == 1 then
+				table.insert(points, v+x)
+			else
+				table.insert(points, v+y)
+			end
+		end					
+		table.insert(allpoints,points)				
+	end
+	return allpoints
+end 
+
+
 gBackgroundDefs = {
 	["oscCircle"] = {
 			init2 = function(self) 
@@ -41,36 +58,28 @@ gBackgroundDefs = {
 						self.x = love.graphics.getWidth()/2
 						self.y = love.graphics.getHeight()/2
 						gBackgroundImage = love.graphics.newImage("graphics/menubackground.png")
-						self.triangles = {{0, math.sqrt(3)*self.y/4, -self.y/2,-math.sqrt(3)*self.y/4,self.y/2,-math.sqrt(3)*self.y/4},
-							{0, math.sqrt(3)*self.y/4, -self.y/2,-math.sqrt(3)*self.y/4,self.y/2,-math.sqrt(3)*self.y/4},
-							{0, math.sqrt(3)*self.y/4, -self.y/2,-math.sqrt(3)*self.y/4,self.y/2,-math.sqrt(3)*self.y/4},}
+						self.triangles = {{0, self.y/2, -self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2,self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2},
+							{0, self.y/2, -self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2,self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2},
+							{0, self.y/2, -self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2,self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2},
+							{0, self.y/2, -self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2,self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2},
+							{0, self.y/2, -self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2,self.y/2 * math.sqrt(3)/2,-self.y/2 * 1/2}}
 						self.angle = 0
 					end,
 			update = function(self, dt)
-						self.angle = (self.angle + dt) % (2 * math.pi)/20
+						self.angle = (self.angle + dt) % (2 * math.pi)
 						for j, u in ipairs(self.triangles) do
-							self.triangles[j] = rotate_object(self.triangles[j],self.angle*j/4)
+							self.triangles[j] = rotate_object(self.triangles[j],math.cos(self.angle)/40*(j-1)/5 + 1/40*1/5 )
 						end 
 					end,
 			render = function(self)
 						love.graphics.setLineWidth(10)
 						love.graphics.setBackgroundColor(currentPalette.background)
+						love.graphics.setColor(currentPalette.gradient)
+						--love.graphics.draw(gBackgroundImage,0,0,0,self.x*2/1920, self.y*2/1080)
 						love.graphics.resetColor()
-						--love.graphics.draw(self.backImage,0,0,0,self.x*2/1920, self.y*2/1080)
-						allpoints = {}
-						for j, u in ipairs(self.triangles) do
-							points = {}
-							for i, v in ipairs(u) do
-								if (i%2) == 1 then
-									table.insert(points, v+self.x)
-								else
-									table.insert(points, v+self.y)
-								end 
-							end
-							table.insert(allpoints,points)
-						end
+						centeredTriangles = center_points(self.x, self.y, self.triangles)
 						love.graphics.setColor(currentPalette.bgObjects)
-						for i, v in ipairs(allpoints) do
+						for i, v in ipairs(centeredTriangles) do
 							love.graphics.polygon("line", v)
 						end
 						love.graphics.resetColor()
@@ -102,19 +111,10 @@ gBackgroundDefs = {
 			render = function(self)
 						allpoints = {}
 						love.graphics.setLineWidth(10)
-						for j, u in ipairs(self.squares) do
-							points = {}
-							for i, v in ipairs(u) do
-								if (i%2) == 1 then
-									table.insert(points, v+self.x)
-								else
-									table.insert(points, v+self.y)
-								end 
-							end
-							table.insert(allpoints,points)
-						end
+						centeredSquares = center_points(self.x, self.y, self.squares)
+
 						love.graphics.setColor(currentPalette.bgObjects)
-						for i, v in ipairs(allpoints) do
+						for i, v in ipairs(centeredSquares) do
 							love.graphics.polygon("line", v)
 							for k, w in ipairs(v) do
 								if (k%2) == 1 and (i < 3) then
@@ -122,6 +122,68 @@ gBackgroundDefs = {
 								end
 							end 
 						end
+						love.graphics.resetColor()
+					end
+	},
+	["circleFlower"] = {
+		init2 = function(self) 
+						self.x = love.graphics.getWidth()/2
+						self.y = love.graphics.getHeight()/2
+						gBackgroundImage = love.graphics.newImage("graphics/menubackground2.png")
+						love.graphics.setBackgroundColor(currentPalette.background)
+						self.circles = {{-self.y/4,-self.y/4, self.y/4,-self.y/4, self.y/4,self.y/4,-self.y/4,self.y/4},
+										rotate_object({-self.y/4,-self.y/4, self.y/4,-self.y/4, self.y/4,self.y/4,-self.y/4,self.y/4}, math.pi/4),
+										rotate_object({-self.y/4,-self.y/4, self.y/4,-self.y/4, self.y/4,self.y/4,-self.y/4,self.y/4}, math.pi/4),
+										{-self.y/4,-self.y/4, self.y/4,-self.y/4, self.y/4,self.y/4,-self.y/4,self.y/4}
+									}
+						self.triangles = {rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 0),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 2*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 3*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 4*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 5*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 6*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 7*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 8*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 9*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 10*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 11*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 12*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 13*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 14*math.pi/8),
+										rotate_object({0, self.y/4, -self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2,self.y/4 * math.sqrt(3)/2,-self.y/4 * 1/2}, 15*math.pi/8),	
+										}
+						self.circleRad = self.y/16
+						love.graphics.resetColor()		
+					end,
+			update = function(self, dt) 
+						for j, u in ipairs(self.circles) do
+							if j%2 == 0 then
+								self.circles[j] = rotate_object(self.circles[j],math.pi/2048)
+							else
+								self.circles[j] = rotate_object(self.circles[j],-math.pi/2048)
+							end
+						end 
+						for i, v in ipairs(self.triangles) do
+							self.triangles[i] = rotate_object(self.triangles[i],math.pi/2048)
+						end
+					end,
+			render = function(self)
+						allpoints = {}
+						love.graphics.setLineWidth(10)
+						centeredCircles = center_points(self.x, self.y, self.circles)
+						centeredTriangles = center_points(self.x, self.y, self.triangles)
+						love.graphics.setColor(currentPalette.bgObjects)
+						for i, v in ipairs(centeredCircles) do
+							for k, w in ipairs(v) do
+								if (k%2) == 1 then
+									love.graphics.circle("line",v[k], v[k+1], self.circleRad)
+								end
+							end 
+						end
+						for i,v in ipairs(centeredTriangles) do
+							love.graphics.polygon("line", v)
+						end 
 						love.graphics.resetColor()
 					end
 	}
