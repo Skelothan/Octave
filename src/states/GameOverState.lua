9491056c1b5c36e3ea512320fb509c87b967da8e
Julia Shuieh <crystallineblade@gmail.com>
2019-11-21 21:46:44 -0500

    changed game over input

--- src/states/GameOverState.lua
+++ src/states/GameOverState.lua
@@ -13,12 +13,13 @@ function GameOverState:enter(params)
 	gAudioPlayer:stopAudio()
 	self.score = params.score or 0;
 	self.fadeTextColor = gCurrentPalette.menuText
-	self.isWon = params.isWon or false
+	--self.isWon = params.isWon or false
+	self.isWon = true
 	self.stopInputTimer = 4
 	self.file = params.file
 
 	self.currChar = 65
-	self.choosingName = true
+	self.choosingName = self.isWon
 	self.scoreName = ""
 	self.maxLetters = 6
 end
@@ -47,14 +48,29 @@ function GameOverState:update(dt)
 		elseif string.len(self.scoreName) ~= 0 and self.currChar == 47 then
 			self.currChar = 122
 		end
-	end
-	if love.keyboard.wasInput("bottomArrow") and self.choosingName then
+	elseif self.choosingName and love.keyboard.wasInput("bottomArrow") then
 		self.scoreName = self.scoreName .. string.char(self.currChar)
 		self.currChar = 97
 		if string.len(self.scoreName) == self.maxLetters then
 			self.choosingName = false
 		end
-	elseif self.stopInputTimer <= 0 and love.keyboard.wasInput("bottomArrow") and not self.choosingName then
+	elseif love.keyboard.wasInput("topArrow") then
+		if string.len(self.scoreName) > 0 then
+			self.choosingName = true
+			self.scoreName = string.sub(self.scoreName, 1, string.len(self.scoreName)-1)
+			if string.len(self.scoreName) == 0 then
+				self.currChar = 65
+			else
+				self.currChar = 97
+			end
+		end
+	end
+	if self.stopInputTimer <= 0 and (love.keyboard.wasInput("togglePauseMenu") or 
+		(not self.choosingName and love.keyboard.wasInput("bottomArrow"))) then
+		
+		if self.choosingName then
+			self.scoreName = self.scoreName .. string.char(self.currChar)
+		end
 		gAudioPlayer:changeAudio(love.audio.newSource("sfx/Welcome_to_Octave.wav", "stream"))
 		gAudioPlayer:setLooping(true)
 		gAudioPlayer:playAudio()
@@ -72,34 +88,36 @@ function GameOverState:render()
 		love.graphics.printf("Game Over", gFonts["AvenirLight64"], 0, love.graphics.getHeight() / 4, love.graphics.getWidth(), "center")
 	end
 
-	love.graphics.printf("Enter your name: ", gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 - 50, love.graphics.getWidth(), "center")
-	
-	love.graphics.rectangle(
-		"line",
-		love.graphics.getWidth()/2 - love.graphics.getWidth() / 12,
-		love.graphics.getHeight() / 2, 
-		love.graphics.getWidth() / 6,
-		50
-	)
+	if self.isWon then
+		love.graphics.printf("Enter your name: ", gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 - 50, love.graphics.getWidth(), "center")
+		love.graphics.rectangle(
+			"line",
+			love.graphics.getWidth()/2 - love.graphics.getWidth() / 12,
+			love.graphics.getHeight() / 2, 
+			love.graphics.getWidth() / 6,
+			50
+		)
+	end
 
 	if(self.choosingName) then
 		love.graphics.printf(self.scoreName .. string.char(self.currChar), 
 			gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 + 3, love.graphics.getWidth(), "center")
-	else
+	elseif self.isWon then
 		love.graphics.printf(self.scoreName, gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 + 3, love.graphics.getWidth(), "center")
 	end
 
-	love.graphics.printf("Your Score: " .. comma_value(self.score), 
-		gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 + love.graphics.getHeight() / 12, love.graphics.getWidth(), "center")
+	if self.isWon then 
+		love.graphics.printf("Your Score: " .. comma_value(self.score), 
+			gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2 + love.graphics.getHeight() / 12, love.graphics.getWidth(), "center")
+	else 
+		love.graphics.printf("Your Score: " .. comma_value(self.score), 
+			gFonts["AvenirLight32"], 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+	end
 
 	if self.stopInputTimer <= 0.5 then
 		if self.fadeTextColor[4] ~= 1 then
 			self.fadeTextColor = 1 - 2 * self.stopInputTimer
 		end
-		if self.choosingName then
-			love.graphics.printf("Press [down triangle] to select", gFonts["AvenirLight32"], 0, love.graphics.getHeight()*0.75, love.graphics.getWidth(), "center")
-		else
-			love.graphics.printf("Press [down triangle] to return", gFonts["AvenirLight32"], 0, love.graphics.getHeight()*0.75, love.graphics.getWidth(), "center")
-		end
+		love.graphics.printf("Press [down triangle] to return", gFonts["AvenirLight32"], 0, love.graphics.getHeight()*0.75, love.graphics.getWidth(), "center")
 	end
 end
